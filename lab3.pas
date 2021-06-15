0424414d7efb9c19a9ceddb68b3cf128703f9233
[George Seletski] <[george.seletski.1224@gmail.com]>
2021-06-16 00:45:39 +0300

    fixed all errors. But it's needed to be done

--- lab3.pas
+++ lab3.pas
@@ -13,7 +13,7 @@ var
   K_old:array[1..4] of double;
   F:array[1..4] of double;
   res:double;
-  s:int;
+  s:integer;
 
 function yPrime(x,y,z:double):double;
  begin
@@ -29,6 +29,61 @@ procedure rkm(h0,x0,y0,z0:double;var xN,yN,zN:double);
      a1,a2,a3:double;
      l1,l2:double;
      h3:double;
+     
+ procedure G_pr(zn:double);
+var m,n:INTEGER;
+    ma:array[1..4,1..4] of DOUBLE;
+    h:double;
+   
+BEGIN //заполняем матрицу G
+  h:=0.05;
+  for m := 1 to 4 do
+  for  n:= 1 to 4 do
+  begin
+    if (m = n) then 
+      begin
+        ma[m,n]:= 1 - (h/4);
+      end
+    else
+      begin
+        
+          ma[1,2]:= -1 * A2 * h;
+          ma[1,3]:= -1 * 0.5 * h;
+          ma[1,4]:= -2 * A2 * h;
+
+          ma[2,1]:= ma[1,2];
+          ma[2,3]:= ma[1,4];
+          ma[2,4]:= ma[1,3];
+
+          ma[3,1]:= -1 * (zn + (h/4)*L1*A2*h*L2)*(h/2);
+          ma[3,2]:= -1 * (zn + (h/4)*L1*A2*h*L2)*(A2*h/2);
+          ma[3,4]:= ma[1,2];
+          
+          ma[4,1]:= -2 * (zn + (h/4)*L1*A2*h*L2)*A2*h;
+          ma[4,2]:= ma[4,1];
+          ma[4,3]:= ma[3,4];
+
+           
+    end;
+   end;
+  //транспонирование матрицы
+  for m := 1 to 4 do
+  for  n:= 1 to 4 do
+  begin
+      if(m <> n) then
+        ma[m,n]:= ma[n,m];
+  end;
+
+ // G^T * Ф^T
+  tmp[1]:=ma[1,1]*F[1] + ma[1,2]*F[2] + ma[1,3]*F[3] + ma[1,4]*F[4];
+  tmp[2]:=ma[2,1]*F[1] + ma[2,2]*F[2] + ma[2,3]*F[3] + ma[2,4]*F[4];
+  tmp[3]:=ma[3,1]*F[1] + ma[3,2]*F[2] + ma[3,3]*F[3] + ma[3,4]*F[4];
+  tmp[4]:=ma[4,1]*F[1] + ma[4,2]*F[2] + ma[4,3]*F[3] + ma[4,4]*F[4];
+
+
+END;
+
+
  begin
    a1:= ( (1/2) - (sqrt(3)/6) );
    a2:= ( (1/4) - (sqrt(3)/6) );
@@ -50,7 +105,7 @@ procedure rkm(h0,x0,y0,z0:double;var xN,yN,zN:double);
    zN:=(h0/2)*(l1_new+l2_new)+z0;
    
    //определяем сначала матрицу G^T а потом произведение G^T * Ф^T
-   G(zN);
+   G_pr(zN);
 
    //K(s)
    K_old[1]:= k1;
@@ -73,69 +128,14 @@ procedure rkm(h0,x0,y0,z0:double;var xN,yN,zN:double);
 
 
 
-function G(zn:double);
-var m,n:INTEGER;
-    ma:array[1..4,1..4] of DOUBLE;
-    h:double;
-   
-BEGIN //заполняем матрицу G
-h:=0.05;
-for m := 1 to 4 do
-  for  n:= 1 to 4 do
-  begin
-    if (m = n) then 
-      begin
-        
-        ma[m,n]:= 1 - (h/4);
-        
-      end
-    else
-      begin
-        
-          ma[1,2]:= -1 * A2 * h;
-          ma[1,3]:= -1 * 0.5 * h;
-          ma[1,4]:= -2 * A2 * h;
-
-          ma[2,1]:= ma[1,2];
-          ma[2,3]:= ma[1,4];
-          ma[2,4]:= ma[1,3];
-
-          m[3,1]:= -1 * (zn + (h/4)*L1*A2*h*L2)*(h/2);
-          m[3,2]:= -1 * (zn + (h/4)*L1*A2*h*L2)*(A2*h/2);
-          m[3,4]:= ma[1,2];
-          
-          ma[4,1]:= -2 * (zn + (h/4)*L1*A2*h*L2)*A2*h;
-          ma[4,2]:= ma[4,1];
-          ma[4,3]:= m[3,4];
-
-           
-    end;
-  //транспонирование матрицы
-  for m := 1 to 4 do
-  for  n:= 1 to 4 do
-  begin
-  if(m <> n) then
-  begin
-      ma[m,n]:= ma[n,m];
-
-  end;
-
- // G^T * Ф^T
-  tmp[1]:=ma[1,1]*F[1] + ma[1,2]*F[2] + ma[1,3]*F[3] + ma[1,4]*F[4];
-  tmp[2]:=ma[2,1]*F[1] + ma[2,2]*F[2] + ma[2,3]*F[3] + ma[2,4]*F[4];
-  tmp[3]:=ma[3,1]*F[1] + ma[3,2]*F[2] + ma[3,3]*F[3] + ma[3,4]*F[4];
-  tmp[4]:=ma[4,1]*F[1] + ma[4,2]*F[2] + ma[4,3]*F[3] + ma[4,4]*F[4];
 
 
-END;
-
-
-
-procedure print(x,y,z:double);
+procedure pr(x,y,z:double);
 var i:integer;
     eps:double;
+    
 begin
-eps:=0.001;
+  eps:=0.001;
   if(res<=eps)then
     begin
      // writeln(K[1]:10:4,'  ',K[2]:10:4,'  ',K[3]:10:4,' ' K[4]:10:4);
@@ -147,26 +147,24 @@ end;
 
 
 begin
-xEnd:= 1;
+xEnd:=1;
 xBegin:=0;
-y0:=2;
-x0:=0;
-z0:=1;
-p:=0;
+yBegin:=1;
+zBegin:=2;
 for s:=2 to 4 do
 begin
    h0:= exp(-s*ln(10));
    x[0]:=xBegin;
-   y[0]:=yBrgin;
+   y[0]:=yBegin;
    z[0]:=zBegin;
-   n:=trunc(xEnd/h);
+   {n:=trunc(xEnd/h);}
    rkm(h,x[0],y[0],z[0],x[1],y[1],z[1]);
    rkm(h,x[1],y[1],z[1],x[2],y[2],z[2]);
    rkm(h,x[2],y[2],z[2],x[3],y[3],z[3]);
-   print(x[0],y[0],z[0]);
-   print(x[1],y[1],z[1]);
-   print(x[2],y[2],z[2]);
-   print(x[3],y[3],z[3]);
+   pr(x[0],y[0],z[0]);
+   pr(x[1],y[1],z[1]);
+   pr(x[2],y[2],z[2]);
+   pr(x[3],y[3],z[3]);
 
    
      
